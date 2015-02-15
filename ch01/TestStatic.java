38193ce1bf8fbb65d308e0d9194d3953ad41b058
Varun Krishna. P <varunkrishna.dbz@gmail.com>
2015-02-15 22:35:28 +0530

    Add TestStatic and TestStaticImports to the repository..

---
+++ ch01/TestStatic.java
@@ -0,0 +1,10 @@
+// Before using static imports
+package ch01;
+public class TestStatic {
+	public static void main(String[] args) {
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.toHexString(42));
+		
+		
+	}
+}
---
+++ ch01/TestStaticImports.java
@@ -0,0 +1,10 @@
+// After applying static import statements to the java program
+package ch01;
+import static java.lang.System.out;
+import static java.lang.Integer.*;
+public static TestStaticImports {
+	public static void main(String[] args) {
+		out.println(MAX_VALUE);
+		out.println(toHexString(42));
+	}
+}

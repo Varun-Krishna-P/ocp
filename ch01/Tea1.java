5cf3e0c63831ead5ce96e8c82d0d0fbf0518d02f
Varun Krishna. P <varunkrishna.dbz@gmail.com>
2015-02-16 07:20:34 +0530

    Final class example.

---
+++ ch01/Tea1.java
@@ -0,0 +1,10 @@
+package ch01;
+import ch01.cert.Beverage1;
+class Tea1 extends Beverage1{}
+
+/*
+ch01\Tea1.java:3: error: cannot inherit from final Beverage1
+class Tea1 extends Beverage1{}
+				   ^
+1 error
+*/
---
+++ ch01/cert/Beverage1.java
@@ -0,0 +1,7 @@
+package ch01.cert;
+//final class can't be inherited in the sub class
+public final class Beverage1{
+	public void importantMethod(){
+		
+	}
+}

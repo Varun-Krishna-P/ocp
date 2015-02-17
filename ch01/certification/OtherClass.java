9e41c2dfebab260f99d97725d8820df7951bad63
Varun Krishna. P <varunkrishna.dbz@gmail.com>
2015-02-18 00:17:39 +0530

    A Program to demonstrate 'package'(default) level access.

---
+++ ch01/certification/OtherClass.java
@@ -0,0 +1,7 @@
+package ch01.certification;
+public class OtherClass{
+	void testIt() {// no modifier so default.
+		System.out.println("Other class");
+		
+	}
+}
---
+++ ch01/somethingelse/AccessClass.java
@@ -0,0 +1,8 @@
+package ch01.somethingelse;
+import ch01.certification.OtherClass;
+class AccessClass{
+	static public void main(String args[]){
+		OtherClass other = new OtherClass();
+		other.testIt();
+	}
+}

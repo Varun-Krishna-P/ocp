11fe4a9d0042942ee8416e60fb872c1bb11ecd28
Varun Krishna. P <varunkrishna.dbz@gmail.com>
2015-02-18 00:04:11 +0530

    A program to demonstrate the private access modifer.

---
+++ ch01/cert/PrivateRoo.java
@@ -0,0 +1,6 @@
+package ch01.cert;
+public class PrivateRoo{
+	private String doRooThings(){
+		return "Private message from Roo";
+	}
+}
---
+++ ch01/notcert/UseARoo.java
@@ -0,0 +1,9 @@
+package ch01.nocert;
+import ch01.cert.PrivateRoo;
+class UseARoo{
+	public void testIt(){
+		PrivateRoo r = new PrivateRoo();
+		System.out.println(r.doRooThings());
+		
+	}
+}

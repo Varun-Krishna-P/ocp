2c3ce0c028dfca64950f369f051cf6a330ddc3a3
Varun Krishna. P <varunkrishna.dbz@gmail.com>
2015-04-03 06:48:19 +0530

    Chapter 02 TestAnimal example.

---
+++ ch02/TestAnimal.java
@@ -0,0 +1,38 @@
+package ch02;
+class Animal{
+	public void eat(){
+		System.out.println("This is the general class for the Animal. When the animal is hungry it eats its food:)");
+		
+	}
+}
+class Horse extends Animal{
+	public void eat(){
+		System.out.println("This method especially belongs to the horse class. It eats grass, hay and whatever whenever it feels hungry");
+		
+	}
+
+}
+class Monkey extends Animal{
+	public void eat(){
+		System.out.println("This method especially belongs to the monkey class. It eats coconut, banana and whatever whenever it feels hungry");
+		
+	}
+	void descMe(){
+		System.out.println("The monkey's come in different colours and shapes. They can walk in two legs, they are intelligent creatures.");
+		
+	}
+}
+public class TestAnimal{
+	public static void main(String[] args) {
+		Animal a = new Animal();
+		a.eat();
+		Animal a2 = new Horse();
+		a2.eat();
+		Animal a3 = new Monkey();
+		a3.eat();
+		//a3.descMe();
+		Monkey m = new Monkey();
+		m.eat();
+		m.descMe();
+	}
+}

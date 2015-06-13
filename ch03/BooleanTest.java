202d903c8fc9af13d45552490fc4f3ffbe893ee6
Varun Krishna. P <varunkrishna.dbz@gmail.com>
2015-06-13 20:53:18 +0530

    Boolean Test program.

---
+++ ch03/BooleanTest.java
@@ -0,0 +1,15 @@
+package ch03;
+class BooleanTest {
+	//boolean result = true;
+	public static void main(String[] args) {
+	// comment the following line if you have an instance variable(non-static) result in the class or make that result variable a static variable, so that I can access it with out creating a object for it.
+	boolean result = true;
+	// comment the following line out if you have result declared inside main method.
+		//if(new BooleanTest().result){
+		if(result){
+			System.out.println("True");
+			
+		}else System.out.println("False");
+		
+	}
+}
